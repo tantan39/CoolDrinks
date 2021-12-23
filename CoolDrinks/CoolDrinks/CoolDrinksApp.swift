524bdad7d86018a892cace39baf8bd9891f53608
Tran Thai Tan <tantan992@gmail.com>
2021-12-23 17:06:14 -0500

    Initialize project

---
+++ CoolDrinks/CoolDrinks/CoolDrinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolDrinksApp.swift
+//  CoolDrinks
+//
+//  Created by Tan Tan on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CoolDrinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
